de010dbf8ba21539b7f59488ec76d71681170c20
Athira M <athiramanu9400@gmail.com>
2021-10-09 15:20:25 +0530

    Create progressbar.js

---
+++ ProgressBar/progressbar.js
@@ -0,0 +1,51 @@
+const createProgressElement = (count) => {
+  
+  const progressBar = document.createElement("progress");
+  progressBar.setAttribute("id", count);
+  progressBar.setAttribute("value", 0);
+  progressBar.setAttribute("max", 100);
+  
+  return progressBar;
+}
+
+const appendProgressBar = (container, count) => {
+  
+  container.appendChild(createProgressElement(count));
+  container.appendChild(document.createElement("br"));
+}
+
+const loadProgressBars = () => {
+  
+  const userInput = document.getElementById("userInput").value;
+  const container = document.getElementById("progressBarContainer");
+  
+  let count = 1;
+  appendProgressBar(container, count);
+  
+  setInterval(() => {
+    const element = document.getElementById(count);
+    
+    if (element.value >= 100) {
+      clearInterval(this);
+    }
+    else {
+      element.value += 3;
+    }
+    
+    if (element.value >= 100 && count < userInput) {
+      appendProgressBar(container, ++count);
+    }
+  },50);
+}
+
+const clearProgressBars = () => {
+  
+  const container = document.getElementById("progressBarContainer");
+  container.innerHTML = "";
+}
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener('click', loadProgressBars);
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener('click', clearProgressBars);
